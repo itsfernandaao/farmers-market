5d0a08e40fe09e7e83575e7f1871958af1e83dc1
Fernanda Andrade <fernandaao@gmail.com>
2022-02-22 19:18:50 -0300

    first commit

---
+++ test/farmers_market/core_test.clj
@@ -0,0 +1,7 @@
+(ns farmers-market.core-test
+  (:require [clojure.test :refer :all]
+            [farmers-market.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
